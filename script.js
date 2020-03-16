b36827c8a3aa2c55312bc8c0393415d53cd5595c
zzVIPz <viperexe@mail.ru>
2020-03-16 00:24:07 -0400

    feat: add interactive for tabs and samples of img

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 window.onload = () => {
   mainNavigationClickHandler();
+  portfolioTabsClickHandler();
+  portfolioSampleClickHandler();
 };
 
+//interactive main navigation
 const mainNavigation = document.querySelector(".navigation");
-const btnSubmit = document.querySelector(".input-submit");
-// const btnClose = document.querySelector(".navigation");
 
 const mainNavigationClickHandler = () => {
   mainNavigation.addEventListener("click", event => {
@@ -16,6 +17,56 @@ const mainNavigationClickHandler = () => {
   });
 };
 
-btnSubmit.addEventListener("click", event => {});
+//interactive portfolio tabs
+const portfolioTabs = document.querySelector(".portfolio-tags");
+const portfolioSamples = document.querySelector(".portfolio-samples");
+let portfolioTabsSelected = document.querySelector(".portfolio-link-active");
 
-console.log(mainNavigation);
+const portfolioTabsClickHandler = () => {
+  portfolioTabs.addEventListener("click", event => {
+    if (
+      event.target.classList.contains("portfolio-link") &&
+      event.target.innerText !== portfolioTabsSelected.innerText
+    ) {
+      portfolioTabsSelected = event.target;
+      portfolioTabs
+        .querySelectorAll(".portfolio-link")
+        .forEach(el => el.classList.remove("portfolio-link-active"));
+      event.target.classList.add("portfolio-link-active");
+      const shuffleArray = getShuffleArray([
+        ...portfolioSamples.querySelectorAll(".portfolio-item")
+      ]);
+      portfolioSamples.innerHTML = "";
+      const documentFragment = document.createDocumentFragment();
+      shuffleArray.forEach(el => documentFragment.appendChild(el));
+      portfolioSamples.appendChild(documentFragment);
+    }
+  });
+};
+
+const getShuffleArray = array => {
+  console.log(1, typeof array);
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+//add a border for a selected image
+let portfolioSampleSelected = undefined;
+console.log("1111", portfolioSampleSelected);
+const portfolioSampleClickHandler = () => {
+  portfolioSamples.addEventListener("click", event => {
+    if (event.target.classList.contains("portfolio-image"))
+      portfolioSamples
+        .querySelectorAll(".portfolio-image")
+        .forEach(el => el.classList.remove("portfolio-image-active"));
+    if (event.target.getAttribute("alt") !== portfolioSampleSelected) {
+      event.target.classList.add("portfolio-image-active");
+      portfolioSampleSelected = event.target.getAttribute("alt");
+    } else {
+      portfolioSampleSelected = undefined;
+    }
+  });
+};
